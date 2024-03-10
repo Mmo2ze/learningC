5e1033669fe59eaee69be58ca5acaa3895fee113
Muhammed Al-Ateeqi <medo.hhh2001@gmail.com>
2024-03-10 20:27:59 +0200

    Calc The annual salary of employee

--- main.cpp
+++ main.cpp
@@ -6,13 +6,47 @@ int Q3(int monthlySalary){
 
     return  monthlySalary * 12;
 }
+int Q5(int liters){
 
-int main() {
-//
-    int monthlySal ;
-    printf("enter your monthly salary : ");
-    scanf("%d",&monthlySal);
-    int annualSalary = Q3(monthlySal);
-    printf("Your Annual Salary is : %d" , annualSalary);
+
+    return liters * 23;
+}
+int Q6(){
+    int temp;
+    int totalStudentSubj;
+    printf("Entre yr first subj :");
+    scanf("%d",&temp);
+    totalStudentSubj += temp;
+    printf("Entre yr 2 subj :");
+    scanf("%d",&temp);
+    totalStudentSubj += temp;
+    printf("Entre yr 3 subj :");
+    scanf("%d",&temp);
+    totalStudentSubj += temp;
+    printf("Entre yr 4 subj :");
+    scanf("%d",&temp);
+    totalStudentSubj += temp;
+    printf("Entre yr 5 subj :");
+    scanf("%d",&temp);
+    totalStudentSubj += temp;
+
+    int missingMarks = 500 - totalStudentSubj;
+    printf("Total Marks : %d\n",totalStudentSubj);
+    printf("Missing Marks : %d",missingMarks);
     return 0;
 }
+
+int Q7( int nOfTables,int nOfChairs){
+
+
+
+    return 30*nOfTables + 25*nOfChairs;
+}
+
+int main() {
+
+
+
+    return 0;
+}
+
